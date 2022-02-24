4df2bf6114067a5e064ab05acb8ad0f64c422ac4
Marcos André <andremarcos967@gmail.com>
2022-02-24 20:06:43 -0300

    incremento de requisição de update e delete

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpStatus,
   Param,
   Post,
+  Put,
   Res,
 } from '@nestjs/common';
 import { Response } from 'express';
@@ -31,5 +33,23 @@ export class CoursesController {
     return res
       .status(HttpStatus.CREATED)
       .json({ message: 'Curso criado com sucesso!', course: createCourse });
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCourse: { name: string; duration: number },
+    @Res() res: Response,
+  ) {
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'Curso atualizado com sucesso!', course: updateCourse });
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string, @Res() res: Response) {
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: `Curso com o id ${id}, apagado com sucesso!` });
   }
 }
