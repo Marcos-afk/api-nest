92b4a8ef14bc8bca9a7ca7a471cb5eca6e89751c
Marcos André <andremarcos967@gmail.com>
2022-02-25 20:46:02 -0300

    finalização mini aplicação parte 1

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -10,44 +10,47 @@ import {
   Res,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { CoursesService } from './courses.service';
+import { Course } from './entities/course.entity';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
   @Get()
   findAll(@Res() res: Response) {
-    return res.status(HttpStatus.OK).json({ message: 'Lista de cursos!' });
+    const courses = this.coursesService.findAll();
+    return res.status(HttpStatus.OK).json(courses);
   }
 
   @Get(':id')
   findById(@Param('id') id: string, @Res() res: Response) {
-    return res
-      .status(HttpStatus.OK)
-      .json({ message: `Curso do id ${id} encontrado!` });
+    const course = this.coursesService.findById(id);
+    return res.status(HttpStatus.OK).json(course);
   }
 
   @Post()
-  create(
-    @Body() createCourse: { name: string; duration: number },
-    @Res() res: Response,
-  ) {
+  create(@Body() createCourse: Course, @Res() res: Response) {
+    this.coursesService.create(createCourse);
     return res
       .status(HttpStatus.CREATED)
-      .json({ message: 'Curso criado com sucesso!', course: createCourse });
+      .json({ message: 'Curso criado com sucesso!' });
   }
 
   @Put(':id')
   update(
     @Param('id') id: string,
-    @Body() updateCourse: { name: string; duration: number },
+    @Body() updateCourse: Course,
     @Res() res: Response,
   ) {
+    this.coursesService.update(id, updateCourse);
     return res
       .status(HttpStatus.OK)
-      .json({ message: 'Curso atualizado com sucesso!', course: updateCourse });
+      .json({ message: 'Curso atualizado com sucesso!' });
   }
 
   @Delete(':id')
   remove(@Param('id') id: string, @Res() res: Response) {
+    this.coursesService.remove(id);
     return res
       .status(HttpStatus.OK)
       .json({ message: `Curso com o id ${id}, apagado com sucesso!` });
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,111 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Redes de computadores',
+      duration: 50,
+      description: 'Curso básico de redes',
+      tags: ['redes de computadores', 'iniciante'],
+    },
+  ];
+
+  findAll() {
+    if (this.courses.length < 1) {
+      throw new HttpException('Lista vazia.', HttpStatus.NOT_FOUND);
+    }
+    return this.courses;
+  }
+
+  findById(id: string) {
+    const course = this.courses.find((course) => course.id === Number(id));
+    if (!course) {
+      throw new HttpException(
+        `Curso com o id ${id}, não foi encontrado.`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return course;
+  }
+
+  create(createCourseDto: any) {
+    const isExistName = this.courses.find(
+      (course) => course.name === createCourseDto.name,
+    );
+
+    if (isExistName) {
+      throw new HttpException(
+        `Curso com o nome ${isExistName.name} já foi cadastrado.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const isExistId = this.courses.find(
+      (course) => course.id === Number(createCourseDto.id),
+    );
+
+    if (isExistId) {
+      throw new HttpException(
+        `Curso com o id ${isExistId.id} já foi cadastrado.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course: Course) => course.id === Number(id),
+    );
+
+    if (indexCourse < 0) {
+      throw new HttpException(
+        `Curso com o id ${id} não foi encontrado.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const isExistName = this.courses.find(
+      (course) => course.name === updateCourseDto.name,
+    );
+
+    if (isExistName && this.courses[indexCourse].id !== isExistName.id) {
+      throw new HttpException(
+        `Curso com o nome ${isExistName.name} já foi cadastrado.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const isExistId = this.courses.find(
+      (course) => course.id === Number(updateCourseDto.id),
+    );
+
+    if (isExistId && this.courses[indexCourse].id !== isExistId.id) {
+      throw new HttpException(
+        `Curso com o id ${isExistId.id} já foi cadastrado.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course: Course) => course.id === Number(id),
+    );
+
+    if (indexCourse < 0) {
+      throw new HttpException(
+        `Curso com o id ${id} não foi encontrado.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    this.courses.splice(indexCourse, 1);
+  }
+}
